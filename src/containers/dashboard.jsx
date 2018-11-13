d332a14b8980457abc17cdf56318e7b3a1b39dfd
harry524483 <harinder.singh@sainsburys.co.uk>
2018-11-13 20:51:30 +0000

    initial version

---
+++ src/containers/dashboard.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { Table } from "antd";
+import { data } from "../misc";
+
+const columns = [
+  {
+    title: "Test Runner ID",
+    dataIndex: "uuid",
+    key: "uuid",
+    render: uuid => <a href="hello">{uuid}</a>
+  },
+  {
+    title: "Device",
+    dataIndex: "device",
+    key: "device",
+    sorter: (a, b) => a.device.length - b.device.length
+  },
+  {
+    title: "OS",
+    dataIndex: "os",
+    key: "os",
+    sorter: (a, b) => a.os.length - b.os.length
+  },
+  {
+    title: "Duration",
+    key: "duration",
+    dataIndex: "duration",
+    sorter: (a, b) => a.duration - b.duration
+  },
+  {
+    title: "Total",
+    key: "total",
+    dataIndex: "total",
+    sorter: (a, b) => a.total - b.total
+  },
+  {
+    title: "Passed",
+    key: "passed",
+    dataIndex: "passed",
+    sorter: (a, b) => a.passed - b.passed
+  },
+  {
+    title: "Failed",
+    key: "failed",
+    dataIndex: "failed",
+    sorter: (a, b) => a.failed - b.failed
+  }
+];
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <Table columns={columns} dataSource={data} />
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
