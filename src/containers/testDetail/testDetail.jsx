3b053afbc01a73ec208b5936fe5e770d6840c285
harry524483 <harinder.singh@sainsburys.co.uk>
2018-11-21 23:03:21 +0000

    add badge for each class

--- src/containers/testDetail/testDetail.jsx
+++ src/containers/testDetail/testDetail.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Collapse, Card, Table } from "antd";
+import { Collapse, Card, Table, Row, Col, Badge } from "antd";
 import { NAME } from "./constant";
 import { fetchDeviceInfo } from "./action";
 import Icon from "../../components/icon/icon";
@@ -45,6 +45,38 @@ const columns = [
   }
 ];
 
+const countPassedOrFailedTest = methodList => {
+  let passed = 0;
+  let failed = 0;
+  methodList.forEach(methodInfo => {
+    if (methodInfo.testResult.toLowerCase() === "pass") {
+      passed++;
+    } else if (methodInfo.testResult.toLowerCase() === "fail") {
+      failed++;
+    }
+  });
+  return { passed, failed };
+};
+
+const headerContent = (className, methodList) => (
+  <div style={{ display: "flex", justifyContent: "space-between" }}>
+    <div>{className}</div>
+    <div style={{ marginRight: "20px" }}>
+      <Badge
+        showZero
+        count={methodList.length}
+        style={{ backgroundColor: "#8470ff", marginRight: "5px" }}
+      />
+      <Badge
+        showZero
+        count={countPassedOrFailedTest(methodList).passed}
+        style={{ backgroundColor: "#228B22", marginRight: "5px" }}
+      />
+      <Badge showZero count={countPassedOrFailedTest(methodList).failed} />
+    </div>
+  </div>
+);
+
 class TestDetail extends Component {
   async componentDidMount() {
     const { fetchDeviceInfo, match } = this.props;
@@ -53,25 +85,63 @@ class TestDetail extends Component {
 
   render() {
     return (
-      <div
-        style={{ background: "#ECECEC", padding: "20px", minHeight: "800px" }}
-      >
-        <Card title="Card title" bordered={false}>
-          <Collapse>
-            {this.props.deviceInfo.map(element => {
-              return (
-                <Panel header={element.className} key={element.key}>
-                  <Table
-                    columns={columns}
-                    dataSource={element.methods}
-                    bordered
-                    pagination={false}
-                  />
-                </Panel>
-              );
-            })}
-          </Collapse>
-        </Card>
+      <div>
+        <Row>
+          <Col span={16}>
+            <div
+              style={{
+                background: "#ECECEC",
+                padding: "20px"
+              }}
+            >
+              <Card title="Card title" bordered={false} />
+            </div>
+          </Col>
+          <Col span={8}>
+            <div
+              style={{
+                background: "#ECECEC",
+                padding: "20px"
+              }}
+            >
+              <Card title="Card title" bordered={false} />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>
+            <div
+              style={{
+                background: "#ECECEC",
+                padding: "0px 20px 20px 20px",
+                minHeight: "700px"
+              }}
+            >
+              <Card title="Card title" bordered={false}>
+                <Collapse>
+                  {this.props.deviceInfo.map(element => {
+                    return (
+                      <Panel
+                        header={headerContent(
+                          element.className,
+                          element.methods
+                        )}
+                        key={element.key}
+                      >
+                        <Table
+                          columns={columns}
+                          dataSource={element.methods}
+                          bordered
+                          pagination={false}
+                        />
+                      </Panel>
+                    );
+                  })}
+                </Collapse>
+              </Card>
+            </div>
+          </Col>
+        </Row>
       </div>
     );
   }
