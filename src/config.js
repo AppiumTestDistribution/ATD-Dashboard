b5d44399d523e1f773c53a6c423946e5ac58f277
saikrishna321 <saikrishna321@yahoo.com>
2019-03-24 17:08:58 +0000

    configure API port

--- src/config.js
+++ src/config.js
@@ -1,20 +1,20 @@
 export const config = {
   local: {
     service: {
-      dashboard: { hostname: "http://localhost", port: 3000 },
-      env: { hostname: "http://localhost", port: 3000 }
+      dashboard: { hostname: "http://localhost", port: process.env.PORT || 3000 },
+      env: { hostname: "http://localhost", port: process.env.PORT || 3000 }
     }
   },
   dev: {
     service: {
-      dashboard: { hostname: "http://localhost", port: 3000 },
-      env: { hostname: "http://localhost", port: 3000 }
+      dashboard: { hostname: "http://localhost", port: process.env.PORT || 3000 },
+      env: { hostname: "http://localhost", port: process.env.PORT || 3000 }
     }
   },
   prod: {
     service: {
-      dashboard: { hostname: "http://localhost", port: 3000 },
-      env: { hostname: "http://localhost", port: 3000 }
+      dashboard: { hostname: "http://localhost", port: process.env.PORT || 3000 },
+      env: { hostname: "http://localhost", port: process.env.PORT || 3000 }
     }
   }
 };
